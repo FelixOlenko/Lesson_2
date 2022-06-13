4cf9eee0d9a31006f57daf207a9525e32ff3d794
Felix Olenko <0feliks0@gmail.com>
2022-06-13 19:19:12 +0300

    3

---
+++ TASK3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+
+
+if (stringNumber.Length < 3) 
+{
+  Console.WriteLine("Третьей цифры заданного числа не существует");
+  
+}
+
+else
+{
+  Console.WriteLine("Третья цифра заданного числа: "+ stringNumber[2]);
+}
